bca5c071279009888b40f725e7b8b07022bfb3c2
Saajid Parker <42733838+ShinobiZA@users.noreply.github.com>
2020-09-25 17:59:48 +0200

    adding usermanual structure

---
+++ user_manual.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+ <html>
+  <head>
+    <meta charset="utf-8">
+    <title>User Manual</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css\manual.css">
+  </head>
+  <body>
+<div class = "container">
+<div class = "row">
+<div class =col>
+<img src="Resources\Logo.png" alt="logo">
+<h2>Tiny Tubbies Edu-care Parent to Teacher Communication Web Applicatio</h2>
+<h1>User Manual</h1>
+<h2>Developed by Plum Inc</h2>
+</div>
+</div>
+<div class = "row">
+<div class = "col">
+<h2>How To Install</h2>
+<p>This system is a web-based application which one can gain access to through logging onto the Tiny Tubbies Website.</p>
+</div>
+</div>
+<div class = "row">
+<div class = "col">
+<h1>Instructions</h1>
+<h2>Landing Page</h2>
+<img src="images\lpic1.png" alt="">
+<img src="images\lpic2.png" alt="">
+<img src="images\lpic3.png" alt="">
+<img src="images\lpic4.png" alt="">
+<p>The Landing page is the first page that users and anyone on the internet can see. It will include sections for pictures, an about us, contact form and the services we offer. Users that are the parents can login by clicking the tab in the top navigation bar called “Login”.</p>
+</div>
+</div>
+<div class = "row">
+<div class = "col">
+<h2>Login Page</h2>
+<img src="images\loginpage.png" alt="">
+<p>Users are greeted with this Sign-in Form that they will put their details into to gain access to our application. An email and password is required and needs to be verified by as so no unauthorised users can have access. If a user needs to still sign-up and create an account they can click on the “Create one” in blue under the Sign in Button. Users who forgot their password will be notified by email when clicking on the “Click here” next to forgot password?</p>
+</div>
+</div>
+
+<div class="footer-copyright text-center py-3">
+    <strong>Copyright © 2020 Tiny Tubbies Educare</strong>
+  </div>
+</div>
+    
+  </body>
+</html>
