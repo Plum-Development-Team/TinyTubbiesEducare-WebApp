e43021c15258be28a717e3eba4e0f2031e946983
Saajid Parker <42733838+ShinobiZA@users.noreply.github.com>
2020-09-25 19:14:34 +0200

    Completed HTML Structure for UserManual Page

--- user_manual.php
+++ user_manual.php
@@ -41,11 +41,60 @@
 <p>Users are greeted with this Sign-in Form that they will put their details into to gain access to our application. An email and password is required and needs to be verified by as so no unauthorised users can have access. If a user needs to still sign-up and create an account they can click on the “Create one” in blue under the Sign in Button. Users who forgot their password will be notified by email when clicking on the “Click here” next to forgot password?</p>
 </div>
 </div>
-
+<div class = "row">
+<div class = "col">
+<h2>Sign-up Page</h2>
+<img src="images\sign-up.png" alt="">
+<p>This sign up page is displayed when users do not yet have an account and need to become a user. The user will fill in their details and be notified when they are verified and can login to the system.</p>
+</div>
+</div>
+<div class = "row">
+<div class = "col">
+<h2>Dashboard</h2>
+<img src="images\dashboardImg.JPG" alt="">
+<p>The Dashboard displays your Task list as well as the classes your kid/s are part of alongside any messages unread in the class. You can also see any and access any unread messages under “Personal Messages” or “Class messages”. Help can also be accessed from the dashboard</p>
+</div>
+</div>
+<div class = "row">
+<div class = "col">
+<h2>Chat</h2>
+<img src="images\chat.png" alt="">
+<p>The Chat page allows you to chat to any contacts privately or you can access the group chats with a similar layout. Chat history is saved, and messages can be deleted and seen if read. Users that you are chatting to can also be seen if they are online. The Profile page can also be accessed through the chat system.</p>
+</div>
+</div>
+<div class = "row">
+<div class = "col">
+<h2>Help Page</h2>
+<img src="images\help-page.png" alt="">
+<p>The help page accessed from the Dashboard will give you details on what to do if you are having any issues with the system.</p>
+</div>
+</div>
+<div class = "row">
+<div class = "col">
+<h2>Contacts List</h2>
+<img src="images\contacts.png" alt="">
+<p>The contacts section accessed through he chat system shows the users that you can chat with. There is also a search function to chat to a specific person by entering their name in the search area.</p>
+</div>
+</div>
+<div class = "row">
+<div class = "col">
+<h2>Profile Page</h2>
+<img src="images\profile1.png" alt="">
+<br>
+<img src="images\profile2.png" alt="">
+<p>The Profile page allows you to see and update your details and profile picture. You will fill in what you want changed and click on “Update” to save the changes.</p>
+</div>
+</div>
+<div class = "row">
+<div class = "col">
+<h2>Repository Page</h2>
+<img src="images\repository.png" alt="">
+<p>This page allows you to upload files to your classes that you are a part of. These can be scanned through class work or forms that need to be handed in. The uploaded files will connect to the Google Drive Folder The teacher has access to.</p>
+</div>
+</div>
 <div class="footer-copyright text-center py-3">
     <strong>Copyright © 2020 Tiny Tubbies Educare</strong>
   </div>
 </div>
-    
   </body>
 </html>
