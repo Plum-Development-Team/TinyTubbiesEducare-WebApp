0e4b5841892d5475bc731af788eeac1e2984489f
Saajid Parker <42733838+ShinobiZA@users.noreply.github.com>
2020-09-28 19:08:00 +0200

    Finished User Manual

--- user_manual.php
+++ user_manual.php
@@ -11,8 +11,8 @@
 <div class = "container">
 <div class = "row">
 <div class =col>
-<img src="Resources\Logo.png" alt="logo">
-<h2>Tiny Tubbies Edu-care Parent to Teacher Communication Web Applicatio</h2>
+<img class = "logo" src="Resources\Logo.png" alt="logo">
+<h2>Tiny Tubbies Edu-care Parent to Teacher Communication Web Application</h2>
 <h1>User Manual</h1>
 <h2>Developed by Plum Inc</h2>
 </div>
@@ -24,75 +24,106 @@
 </div>
 </div>
 <div class = "row">
-<div class = "col">
+<div class = "col" id= "Landing">
 <h1>Instructions</h1>
+<hr>
 <h2>Landing Page</h2>
+<hr>
+<p> The landing page is the very first page a user will see on the internet.<br>
+Sections for pictures, an about us, contact form and the services that we offer is included.<br>
+</p>
 <img src="images\lpic1.png" alt="">
 <img src="images\lpic2.png" alt="">
 <img src="images\lpic3.png" alt="">
 <img src="images\lpic4.png" alt="">
-<p>The Landing page is the first page that users and anyone on the internet can see. It will include sections for pictures, an about us, contact form and the services we offer. Users that are the parents can login by clicking the tab in the top navigation bar called “Login”.</p>
 </div>
 </div>
+<br>
+<hr>
 <div class = "row">
 <div class = "col">
 <h2>Login Page</h2>
+<hr>
+<p> On the top navigation bar of the landing page, click login, you will be redirected to the login page.<br>
+To continue further, you must enter your email address and password.<br>
+If you have forgotten your password,click on the "forgot password" link. </p>
 <img src="images\loginpage.png" alt="">
-<p>Users are greeted with this Sign-in Form that they will put their details into to gain access to our application. An email and password is required and needs to be verified by as so no unauthorised users can have access. If a user needs to still sign-up and create an account they can click on the “Create one” in blue under the Sign in Button. Users who forgot their password will be notified by email when clicking on the “Click here” next to forgot password?</p>
 </div>
 </div>
+<br>
+<hr>
 <div class = "row">
 <div class = "col">
 <h2>Sign-up Page</h2>
+<hr>
+<p> If you do not have an account you may click the sign up link on the login page.<br> When the sign up page loads, enter your personal details.<br> You will recieve an email for your account to be verified. Once your account is verified, you will be taken to the dashboard.</p>
 <img src="images\sign-up.png" alt="">
-<p>This sign up page is displayed when users do not yet have an account and need to become a user. The user will fill in their details and be notified when they are verified and can login to the system.</p>
 </div>
 </div>
+<br>
+<hr>
 <div class = "row">
 <div class = "col">
 <h2>Dashboard</h2>
+<hr>
+<p> The classes that your child/children belong to will display here. As well as your task lists and any messages that are unread will show.<br> You may access maessaged under both personal messages wor class messages. Help may be acessed on the dashboard.</p>
 <img src="images\dashboardImg.JPG" alt="">
-<p>The Dashboard displays your Task list as well as the classes your kid/s are part of alongside any messages unread in the class. You can also see any and access any unread messages under “Personal Messages” or “Class messages”. Help can also be accessed from the dashboard</p>
 </div>
 </div>
+<br>
+<hr>
 <div class = "row">
 <div class = "col">
 <h2>Chat</h2>
+<hr>
+<p> Private chats between you and your contacts are allowed as well as you you can access group chats that has a similar layout.<br> All chat history is saved and messages may be deleted.<br> Messages will display "seen" when read. When a user is online, you may see the "online" icon. <br>Users profiles on the profile page may be acessed through the chat system.</p>
 <img src="images\chat.png" alt="">
-<p>The Chat page allows you to chat to any contacts privately or you can access the group chats with a similar layout. Chat history is saved, and messages can be deleted and seen if read. Users that you are chatting to can also be seen if they are online. The Profile page can also be accessed through the chat system.</p>
 </div>
 </div>
+<br>
+<hr>
 <div class = "row">
 <div class = "col">
 <h2>Help Page</h2>
-<img src="images\help-page.png" alt="">
+<hr>
 <p>The help page accessed from the Dashboard will give you details on what to do if you are having any issues with the system.</p>
+<img src="images\help-page.png" alt="">
 </div>
 </div>
+<br>
+<hr>
 <div class = "row">
 <div class = "col">
 <h2>Contacts List</h2>
+<hr>
+<p> Contacts may be acessed through the chat page. <br> You may search a contact that you would like to chat to by entering their names in the search area. </p>
 <img src="images\contacts.png" alt="">
-<p>The contacts section accessed through he chat system shows the users that you can chat with. There is also a search function to chat to a specific person by entering their name in the search area.</p>
 </div>
 </div>
+<br>
+<hr>
 <div class = "row">
 <div class = "col">
 <h2>Profile Page</h2>
+<hr>
+<p>The Profile page allows you to see and update your details and profile picture. You will fill in what you want changed and click on “Update” to save the changes.</p>
 <img src="images\profile1.png" alt="">
 <br>
 <img src="images\profile2.png" alt="">
-<p>The Profile page allows you to see and update your details and profile picture. You will fill in what you want changed and click on “Update” to save the changes.</p>
 </div>
 </div>
+<br>
+<hr>
 <div class = "row">
 <div class = "col">
 <h2>Repository Page</h2>
+<hr>
+<p> Any files that you may need to upload such as forms that need to be returned can be uploaded to the classes that you are part of.<br> All upload files are connected to the google drive folder which the teacher has access to. </p>
 <img src="images\repository.png" alt="">
-<p>This page allows you to upload files to your classes that you are a part of. These can be scanned through class work or forms that need to be handed in. The uploaded files will connect to the Google Drive Folder The teacher has access to.</p>
 </div>
 </div>
-<div class="footer-copyright text-center py-3">
+<br>
+<div class="footer">
     <strong>Copyright © 2020 Tiny Tubbies Educare</strong>
   </div>
 </div>
